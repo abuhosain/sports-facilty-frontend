92dc3351be7edb2708d67260d1e5bb992496e05d
Abu Hosain <crypticmin925@gmail.com>
2024-09-08 22:34:35 +0600

    support secton daone

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import HowItWorks from "./HowItWorks"
+import Support from "./Support"
 
  
 
@@ -6,6 +7,7 @@ const Home = () => {
   return (
     <div>
       <HowItWorks />
+      <Support />
     </div>
   )
 }
---
+++ src/pages/Home/Support.tsx
@@ -0,0 +1,32 @@
+import { Button } from "antd";
+import { Link } from "react-router-dom";
+
+const Support = () => {
+  return (
+    <div className="grid md:grid-cols-2  h-72">
+      <div className="bg-[#415364] w-full text-white flex flex-col justify-center pl-6">
+        <h3 className="text-4xl">Join the team</h3>
+        <p className="text-2xl">
+          Live the mission of improving communities through sports.
+        </p>
+        <a
+          target="_blank"
+          href="https://sportsfacilities.com/about/employment-opportunities/"
+        >
+          <Button className="w-1/4">LEARN MORE</Button>
+        </a>
+      </div>
+      <div className="bg-[#ebebeb] w-full text-[#415364] flex flex-col justify-center pl-6">
+        <h3 className="text-4xl">Get in touch.</h3>
+        <p className="text-2xl">
+        Connect with our team and learn how we can work together.
+        </p>
+         
+         <Link to={"/contact"}> <Button className="w-1/4">LET'S GO</Button></Link>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Support;
